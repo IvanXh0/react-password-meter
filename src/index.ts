e3a0493bbf73b9617950b4f43ad1c02e16cfad8c
IvanXh0 <ivan.apostolovski@gmail.com>
2024-11-06 04:06:15 +0100

    feat: add tsup

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { PasswordStrengthMeter } from "./components/PasswordStrengthMeter";
+export type {
+  PasswordStrengthMeterProps,
+  ThemeConfig,
+  StrengthLevel,
+} from "./types";
+export { calculateStrength, getStrengthLevel } from "./utils/strength";
